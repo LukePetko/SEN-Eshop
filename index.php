c7e301e8848bb699faf35de0b7a0ef09fb134b42
Lukas Petko <luke.petko@gmail.com>
2018-11-16 18:54:21 +0100

    tvorenie súborov

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Super e-shop</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <h1>Hello World!!</h1>
+</body>
+</html>
