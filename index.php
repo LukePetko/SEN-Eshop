c4e6916b522ca02e40c8cef0230e7b10c129058f
Lukas Petko <luke.petko@gmail.com>
2018-11-16 21:31:35 +0100

    základná štruktúra v index.php

--- index.php
+++ index.php
@@ -2,11 +2,55 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>Super e-shop</title>
+    <title>iShop</title>
     <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
     <script src="js/main.js"></script>
 </head>
 <body>
-    <h1>Hello World!!</h1>
+    <nav id = "side-nav">
+        <a href="">meno.</a>
+        <a href="">prihlásiť sa.</a>
+        <a href="">výpredaj.</a>
+        <a href="">kontakt.</a>
+        <a href="">košík.</a>
+    </nav>
+    <nav id = "top-nav">
+        <a href="">macbook.</a>
+        <a href="">imac.</a>
+        <a href="">ipad.</a>
+        <a href="">iphone.</a>
+        <a href="">apple tv.</a>
+    </nav>
+
+    <div id = "content">
+
+        <div id = "top">
+            <div>
+                <div class = "name">
+                    <p>logo <br> logo</p>
+                        <p>iShop</p>
+                </div>
+                <div class = "product-long">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam debitis illum adipisci doloremque sit quae dolore perferendis minima esse officia quis repudiandae at laborum eum, suscipit hic sed? Maxime, eligendi!</p>
+                </div>
+            </div>
+            <div>
+                <div class = "product-long">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam debitis illum adipisci doloremque sit quae dolore perferendis minima esse officia quis repudiandae at laborum eum, suscipit hic sed? Maxime, eligendi!</p>
+                </div>
+                <div class = "product-short">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam debitis illum adipisci doloremque sit quae dolore perferendis minima esse officia quis repudiandae at laborum eum, suscipit hic sed? Maxime, eligendi!</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <footer>
+        <p>
+            Toto nie je pravý eshop. pre viac informácií klinki <a href = "">sem.</a>
+        </p>
+    </footer>
+
 </body>
 </html>
