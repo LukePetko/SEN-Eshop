6ae0d7d0b919292e75072ffec24ffa716d2dacc7
Lukas Petko <luke.petko@gmail.com>
2018-11-18 13:02:21 +0100

    Prvý content

--- index.php
+++ index.php
@@ -31,7 +31,15 @@
                         <p>iShop</p>
                 </div>
                 <div class = "product-long">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam debitis illum adipisci doloremque sit quae dolore perferendis minima esse officia quis repudiandae at laborum eum, suscipit hic sed? Maxime, eligendi!</p>
+                    <a href = "">
+                        <div class = "product-long-image-section">
+                            <img src="/img/iphone_xs/01.png" alt="iPhone XS">                   
+                        </div>
+                        <div class = "product-long-section">
+                            <h3>iphone xs</h3>
+                            <p>Super Retina, špeciálne navrhnutá obrazovka iPhonu XS má HDR, dokonale temnú čiernu a najvernejšie podanie farieb na trhu OLED displejov. iPhone XS je skonštruovaný z mimoriadnych materiálov. Má vôbec to najodolnejšie sklo na smartphone. Prichádza s nádhernou novou zlatou, s precízne spracovaným rámčekom z chirurgickej ocele a má vyššiu odolnosť proti vode a prachu. Sklenená zadná strana umožňuje iPhone XS nabíjať bezdrôtovo a ešte rýchlejšie než iPhone X. Pozri sa na svoj iPhone XS, keď miesto hesla používaš svoju tvár, všetko bude jednoduchšie. Jedným pohľadom iPhone odomkneš, prihlásiš sa do aplikácií a k účtom. Je to najbezpečnejšie riešenie overovania užívateľa podľa tváre v smartphone. Teraz navyše ešte rýchlejšie. S novým iPhone prichádza aj nový výkon. Ten zabezpečí inteligentný čip A12 Bionic. Toto je najchytrejší a najvýkonnejší čip v smartphone s Neural Enginom ďalšej generácie. Prináša pôsobivé zážitky v rozšírenej realite. Fantastické portréty s ovládaním hĺbky ostrosti. A rýchlosť a plynulosť vo všetkom, čo robíš. Najobľúbenejší fotoaparát na svete spúšťa novú éru fotografie. Inovatívny snímač, obrazový signálový procesor a Neural Engine ti teraz pomáhajú fotiť ako nikdy predtým. A ešte niečo. iPhone XS podporuje 4G LTE Advanced s ohromne rýchlym sťahovaním dát. A má až 512GB úložisko, najväčší zo všetkých iPhonov.</p>                    
+                        </div>
+                    </a>
                 </div>
             </div>
             <div>
@@ -39,7 +47,10 @@
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam debitis illum adipisci doloremque sit quae dolore perferendis minima esse officia quis repudiandae at laborum eum, suscipit hic sed? Maxime, eligendi!</p>
                 </div>
                 <div class = "product-short">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam debitis illum adipisci doloremque sit quae dolore perferendis minima esse officia quis repudiandae at laborum eum, suscipit hic sed? Maxime, eligendi!</p>
+                    <a href = "">
+                        <p>apple watch series 4</p>                 
+                        <img src="/img/apple_watch_4/01.png" class = "product-image" alt="Apple Watch Series 4">
+                    </a>
                 </div>
             </div>
         </div>
